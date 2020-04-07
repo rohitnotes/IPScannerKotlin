3fa7954739de6dc3fdda7f759026b9bc9f075802
Wildan Kurniadi <danprogramming10@gmail.com>
2020-04-06 17:54:41 -0700

    #3 Read SSID, added some Permissions in manifest

--- app/src/main/java/com/example/ipscannerk/MainActivity.kt
+++ app/src/main/java/com/example/ipscannerk/MainActivity.kt
@@ -1,20 +1,70 @@
 package com.example.ipscannerk
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.NetworkInfo.DetailedState
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.example.ipscannerk.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(toolbar)
+        displaySSIDName()
+    }
 
+
+    /**
+     * ask Location Permission for getting SSID Name
+     */
+    private fun displaySSIDName() {
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) { //Permission already granted, check the SSID
+            getWifiName()
+        } else { //Location Permission has not been granted
+//provide additional rationale to the user if permission was not granted and the user would benefit from additional context for the use of the permission
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Location Permission is needed to get SSID Name",
+                    Toast.LENGTH_SHORT
+                ).show()
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+                ) //ask the permission again :)
+            } else {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+                ) //ask the permission again :)
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,5 +81,44 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            //Received permission result for ACCESS_FINE_LOCATION
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                binding.contentMain.tvSsidTitle.text = getWifiName()
+            }else{
+                // ACCESS_FINE_LOCATION permission was denied, so we cannot get the SSID Name
+                Toast.makeText(
+                    this@MainActivity,
+                    "Please grant the location permission so we can check the SSID",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun getWifiName(): String {
+        var ssidName = "SSID Name"
+        val manager =
+            applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        if (manager.isWifiEnabled) {
+            val wifiInfo = manager.connectionInfo
+            if (wifiInfo != null) {
+                val state =
+                    WifiInfo.getDetailedStateOf(wifiInfo.supplicantState)
+                if (state == DetailedState.CONNECTED || state == DetailedState.OBTAINING_IPADDR) {
+                    ssidName = wifiInfo.ssid
+                }
+            }
+        }
+        return ssidName
     }
 }
